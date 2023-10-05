ca8b67ca09dacb134e9bc675fd311633b949a68a
zagorny95 <krzysztof.podworny@wp.pl>
2023-10-05 02:33:38 +0200

    cleanup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        
+      </header>
+      <nav>
+
+      </nav>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  );
+}
+
+export default App;
